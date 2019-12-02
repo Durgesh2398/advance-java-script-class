0aae50cc5171b30375116003e26a8234bd998c47
Durgesh2398 <2308durgesh@gmail.com>
2019-12-03 00:03:05 +0530

    5

--- arrow.js
+++ arrow.js
@@ -36,3 +36,22 @@ hello = () => {
   }
 window.addEventListener("load", hello);
 document.getElementById("btn").addEventListener("click", hello);
+
+// var plusOne = [1,2,3,4,5].map(function(num){ return num + 1 });
+// ES6
+var plusOne = [1,2,3,4,5].map(num => num + 1); 
+console.log(plusOne);
+
+
+// ES5
+function Counter(){
+    this.seconds = 0;
+    window.setInterval(function() {
+     this.seconds++;
+   }.bind(this), 1000);
+ }
+ //ES6
+ function Counter(){
+    this.seconds =0;
+    window.setInterval( () => this.seconds++,1000 );
+ }
